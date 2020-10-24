3f6bb07672242b9f2c0123e0a6ad3a25df31a298
Kacper <46610139+Nsquik@users.noreply.github.com>
2020-10-24 21:26:14 +0200

    ADD httpLink and authLink so apollo adds user api key to every request.

--- resources/apollo/index.ts
+++ resources/apollo/index.ts
@@ -1,7 +1,17 @@
-import { ApolloClient, InMemoryCache } from "@apollo/client";
-import { API_URL } from "@env";
+import { ApolloClient, InMemoryCache, createHttpLink } from "@apollo/client";
+import { setContext } from "@apollo/client/link/context";
+import { API_URL, API_KEY } from "@env";
+
+const httpLink = createHttpLink({ uri: API_URL });
+
+const authLink = setContext((_, { headers }) => ({
+  headers: {
+    ...headers,
+    authorization: API_KEY ? `Bearer ${API_KEY}` : "",
+  },
+}));
 
 export const client = new ApolloClient({
-  uri: API_URL,
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache(),
 });
