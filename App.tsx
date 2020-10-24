37467be80b8aaff1ea7cd58c359bc4c2c5a809a0
Kacper <46610139+Nsquik@users.noreply.github.com>
2020-10-24 19:20:57 +0200

    ADD react-native-dotenv to project.
    Added dotenv support to the project, had to declare module in env.d.ts.

--- App.tsx
+++ App.tsx
@@ -1,8 +1,10 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { API_KEY } from "@env";
 
 export default function App() {
+  console.log(API_KEY);
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
@@ -14,8 +16,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
