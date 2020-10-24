b1e92de3e4c04d83d316611a58c7aacacb669d0b
Kacper <46610139+Nsquik@users.noreply.github.com>
2020-10-24 21:34:26 +0200

    LITTLE REFACTOR - move httpLink to separate file

--- resources/apollo/index.ts
+++ resources/apollo/index.ts
@@ -1,15 +1,6 @@
-import { ApolloClient, InMemoryCache, createHttpLink } from "@apollo/client";
-import { setContext } from "@apollo/client/link/context";
-import { API_URL, API_KEY } from "@env";
+import { ApolloClient, InMemoryCache } from "@apollo/client";
 
-const httpLink = createHttpLink({ uri: API_URL });
-
-const authLink = setContext((_, { headers }) => ({
-  headers: {
-    ...headers,
-    authorization: API_KEY ? `Bearer ${API_KEY}` : "",
-  },
-}));
+import { authLink, httpLink } from "./httpLink";
 
 export const client = new ApolloClient({
   link: authLink.concat(httpLink),
